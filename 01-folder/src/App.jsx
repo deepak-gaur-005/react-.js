916c6318d5fbe3fcf9c80583a87489beb1ee7c06
Deepak Gaur <your-email@gmail.com>
2026-04-06 23:21:27 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <> // fragments (empty tags)
+      <div id='dad'>
+        <h1 id='child1'>hello  I'm react</h1>
+        <h3 id='child2'> kaise hey app log</h3>
+      </div>
+      <div id="chacha"> </div>
+    </>
+  )
+}
+
+export default App
+
