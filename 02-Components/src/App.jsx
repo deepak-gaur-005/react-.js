c89590ea808d46267adf40bc4d3face1d268c6ae
Deepak Gaur <your-email@gmail.com>
2026-04-07 21:17:14 +0530

    Added 02-Components folder

---
+++ 02-Components/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Card from './components/card'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Navbar />
+      <div className='card'>
+        <h1>absadjjsa</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias!</p>
+      </div>
+      <Card />
+      <Card />
+      <Card />
+      
+    </div>
+
+  )
+}
+
+export default App
---
+++ 02-Components/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='nav'> 
+      this is navbar
+    </div>
+  )
+}
+
+export default Navbar
