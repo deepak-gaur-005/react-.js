0664fd74c8636e819804ef7114639a17705894f7
Deepak Gaur <your-email@gmail.com>
2026-04-10 19:29:32 +0530

    tailwind setup

---
+++ 06-tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='h-screen w-full bg-grey-900 text-white p-4 rounded-lg'></div>
+    </div>
+  )
+}
+
+export default App
